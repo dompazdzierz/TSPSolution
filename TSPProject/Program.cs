881162cf4b25abcf0f439d42e24fba64bbdf7829
dompazdzierz <pazdzierzdominik@protonmail.com>
2020-03-21 18:56:18 +0100

    Update Program.cs

--- TSPProject/Program.cs
+++ TSPProject/Program.cs
@@ -184,7 +184,7 @@ namespace TSPProject
             for(int i = 0; i < 10; i++)
             {
                 recordsLists[i] = resolver.ResolveProblem();
-                Console.WriteLine(i + "___" + resolver.Name);
+                Console.WriteLine(i + "___" + problem.FileName + "_" + resolver.Name);
             }
 
             string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
